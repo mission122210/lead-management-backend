b2144a9fbd438b1c6e61b14374aaa7c8d7dab3ef
mission122210 <gz707985@gmail.com>
2025-10-03 19:05:02 -0400

    initialize

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const MONGO_URI = "mongodb+srv://goku012:0121452goku@cluster0.53byfkj.mongodb.net/master" 
+const MONGO_URI = "mongodb+srv://goku012:0121452goku@cluster0.53byfkj.mongodb.net/jutt" 
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI, {
